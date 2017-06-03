f6e229a9946a9a3e83d9b3950d89ada22b83f21b
Nathaniel Watson <nathaniel@nwatson.nz>
2017-06-03 16:39:51 +1200

    Add basic cannon and bullets

--- src/objects/Cannon.js
+++ src/objects/Cannon.js
@@ -4,16 +4,58 @@
  *
  */
 
+import Bullet from './Bullet.js';
+
 export default class Cannon extends Phaser.Sprite {
 
-  constructor(game, x, y) {
-    super(game, x, y, 'phaser');
+  constructor(state, x, y) {
+    super(state, x, y, 'phaser');
+    this._state = state;
 
     this.anchor.set(0.5);
+    this.width = 100; // TODO: remove
+    this.height = 100; // TODO: remove
+
+    this._maxBullets = 50;
+    this._bulletSpacing = 350;
+    this._bulletSpeed = 800;
+    this._lastFired = -1;
+
+    this.createBulletPool();
   }
 
   update() {
-    // TODO: Stub.
+    this.rotation = this._state.physics.arcade.angleToPointer(this) - (Math.PI * 1.5);
+
+    if (this._state.input.activePointer.isDown) {
+      this.fire();
+    }
   }
 
+  setTarget(x, y) {
+    const o = this.x - x;
+    const a = this.y - y;
+
+    const angle = Math.atan(o/a) * 180 / Math.PI;
+    this.angle = -angle;
+  }
+
+  createBulletPool() {
+    this._bulletPool = this._state.add.group();
+    this._bulletPool.classType = Bullet;
+    this._bulletPool.enableBody = true;
+    this._bulletPool.physicsBodyType = Phaser.Physics.ARCADE;
+    this._bulletPool.createMultiple(this._maxBullets, 'phaser');
+    this._bulletPool.setAll('checkWorldBounds', true);
+    this._bulletPool.setAll('outOfBoundsKill', true);
+  }
+
+  fire() {
+    if (this._state.time.now >= this._lastFired + this._bulletSpacing && this._bulletPool.countDead() > 0) {
+      this._lastFired = this._state.time.now;
+      const bullet = this._bulletPool.getFirstDead();
+      bullet.reset(this.x, this.y);
+      this._state.physics.arcade.moveToPointer(bullet, this._bulletSpeed);
+    }
+  }
 }
--- src/states/Game.js
+++ src/states/Game.js
@@ -5,18 +5,17 @@
  * A sample Game state, displaying the Phaser logo.
  */
 
-import Logo from '../objects/Logo';
 import Cannon from '../objects/Cannon';
 
 export default class Game extends Phaser.State {
 
   create(game) {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.stage.backgroundColor = '#df8a26';
 
     // TODO: Replace this with a really cool game code here :)
-    const {centerX: x, centerY: y} = this.world;
-    this.add.existing(new Logo(this.game, x, y));
-
-    this.add.existing(new Cannon(this.game, x, y));
+    const {centerX: x, height: y} = this.world;
+    this.add.existing(new Cannon(this, x, y));
   }
 }
