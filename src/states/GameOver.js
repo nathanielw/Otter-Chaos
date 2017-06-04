188ac4f5a28bc56b34adca7880f8e47649bb0b56
Nathaniel Watson <nathaniel@nwatson.nz>
2017-06-05 10:28:37 +1200

    Add some end game messages

--- src/states/GameOver.js
+++ src/states/GameOver.js
@@ -4,18 +4,33 @@
  *
  */
 
+const messages = [
+  'FANTAstic effort',
+  'That was SODApressing to watch',
+  'Orange you glad you played?',
+  'Don\'t lose your fizz, try again!',
+  'That was an otterly terrible effort',
+];
+
+const PADDING = 20;
+
 export default class GameOver extends Phaser.State {
 
   create(game) {
-    const style = { font: 'bold 60pt Amethysta', fill: '#ffb365', align: 'center', wordWrap: true, wordWrapWidth: game.world.width };
-    const text = game.add.text(game.world.centerX, game.world.centerY, 'Game Over', style);
+    // TODO: make a function for adding text
+    const style = { font: 'bold 60pt Amethysta', fill: '#ffb365', align: 'center', wordWrap: true, wordWrapWidth: game.world.width - (PADDING * 2) };
+    const text = game.add.text(game.world.centerX, game.world.height  * 0.2, 'Game Over', style);
     text.lineSpacing = -20;
-    text.anchor.set(0.5);
+    text.anchor.set(0.5, 0);
 
-    const smallStyle = { font: '30pt Amethysta', fill: '#ffb365', align: 'center', wordWrap: true, wordWrapWidth: game.world.width };
-    const smallText = game.add.text(game.world.centerX, game.world.height * 0.9, 'Tap to restart', smallStyle);
-
+    const smallStyle = { font: '30pt Amethysta', fill: '#6966aa', align: 'center', wordWrap: true, wordWrapWidth: game.world.width - (PADDING * 2) };
+    const smallText = game.add.text(game.world.centerX, game.world.height * 0.95, 'Tap to restart', smallStyle);
     smallText.anchor.set(0.5, 1);
+
+    const punMessage = messages[Math.floor(Math.random() * messages.length)];
+    const punStyle = { font: '20pt Amethysta', fill: '#fabf83', align: 'center', wordWrap: true, wordWrapWidth: game.world.width - (PADDING * 2) };
+    const punText = game.add.text(game.world.centerX, game.world.height * 0.65, punMessage, punStyle);
+    punText.anchor.set(0.5, 0);
 
     const playButton = this.game.add.button(0, 0, '', () => {
       game.state.start('Game', true, false);
