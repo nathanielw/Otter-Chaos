42a425fc0f62c99d25036b5226d2fc5ea886751b
Nathaniel Watson <nathaniel@nwatson.nz>
2017-06-05 10:48:12 +1200

    Remove splash screen

--- src/assets.js
+++ src/assets.js
@@ -9,9 +9,6 @@
 export default {
   // -- Splash screen assets used by the Preloader.
   boot: [{
-    key: 'splash-screen',
-    type: 'image'
-  }, {
     key: 'progress-bar',
     type: 'image'
   }],
